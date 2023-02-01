4575ea73b05d61ac12bbca5f18559d633976fb40
Parker Kugler <pkugler@ou.edu>
2023-01-31 20:12:01 -0600

    Started writing the GetAisle() function

--- P_WPF_ClassesAndFiles/P_WPF_ClassesAndFiles/MainWindow.xaml.cs
+++ P_WPF_ClassesAndFiles/P_WPF_ClassesAndFiles/MainWindow.xaml.cs
@@ -32,6 +32,10 @@ namespace P_WPF_ClassesAndFiles
             {
                 Toy t = new Toy();
                 string[] toyData = item.Split(',');
+                t.Manufacturer = toyData[0];
+                t.Name = toyData[1];
+                t.Price = Convert.ToDouble(toyData[2]);
+                t.Image = toyData[3];
                 listToys.Items.Add(t);
             }
         }
@@ -40,11 +44,11 @@ namespace P_WPF_ClassesAndFiles
         {
             Toy selectedToy = (Toy)listToys.SelectedItem;
 
-            string[] url = selectedToy[3]
+            string url = selectedToy.Image;
 
             var uri = new Uri(url);
             var img = new BitmapImage(uri);
-            
+            toyImage.Source = img;
 
         }
     }
--- P_WPF_ClassesAndFiles/P_WPF_ClassesAndFiles/Toy.cs
+++ P_WPF_ClassesAndFiles/P_WPF_ClassesAndFiles/Toy.cs
@@ -9,16 +9,31 @@ namespace P_WPF_ClassesAndFiles
     public class Toy
     {
         public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+        private string Aisle;
+
 
         public Toy()
         {
+            Manufacturer = string.Empty;
             Name = string.Empty;
-
+            Price = 0;
+            Image = string.Empty;
+            Aisle = string.Empty;
         }
 
         public override string ToString()
         {
             return Name;
+        }
+
+        public string GetAisle()
+        {
+            string toyAisle = Manufacturer[0].ToUpper() + Price.ToString().Replace(".","");
+
+            return toyAisle;
         }
     }
 }
